3b1c5a04c0db40933e8cb0de058d7c7ebc62565d
Diamond-Share <mikailpira@gmail.com>
2025-10-16 19:46:44 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,23 @@
-/**
- * Warning: Before you edit this file or merge with other current worker
- * In case of edit or rename, merge with other: be careful existing projects with subscribers registrations on this worker path, can be destroyed after changes and you may occur some issues with deliverability
- * In case of two workers: be sure that you provide other file name and update project in PushPushGo Dashboard and use /ppg scope.
- * If you have any questions before you take any action please contact with support@pushpushgo.com for consulting service worker change strategy
- */
-importScripts('https://s-eu-1.pushpushgo.com/68e7a847285710d0ea75a08d/worker.js');
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: data.icon || '/icon.png',
+        badge: '/badge.png',
+        vibrate: [100, 50, 100],
+        data: {
+            url: data.url || '/'
+        }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
+    );
+});
